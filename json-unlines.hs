306ec381779f54a5fd91e25f0da899e85d46b7a7
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2010-09-06 08:45:50 +0100

    Add json-unlines.hs

---
+++ json-unlines.hs
@@ -0,0 +1,15 @@
+import Text.JSON.AttoJSON
+import qualified Data.ByteString.Char8 as S
+import Control.Arrow (second)
+
+main :: IO ()
+main = S.putStr . showJSON          -- printing the output
+     . JSArray                      -- building the result
+     . map err                      -- error handling
+     . map (second parseJSON)       -- parsing a line
+     . zip [1..]                    -- number lines
+     . S.lines                      -- cut into a list of lines
+     =<< S.getContents              -- reading the input
+  where
+  err (ln, Left msg) = error $ "JSON parsing: (line "++show ln++"): "++msg
+  err (_,  Right x)  = x
