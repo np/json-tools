1bda64e848171528ff61089c9802b29174c3be3b
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2010-12-30 18:53:22 +0100

    Add json-deep-select-key.hs

---
+++ json-deep-select-key.hs
@@ -0,0 +1,22 @@
+import qualified Data.Map as M
+import Text.JSON.AttoJSON
+import System.Environment (getArgs)
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
+
+deepSelectKey :: S.ByteString -> JSValue -> [JSValue]
+deepSelectKey key (JSArray vs)
+  = concatMap (deepSelectKey key) vs
+deepSelectKey key (JSObject o)
+  = maybe (concatMap (deepSelectKey key) (map snd (M.toList o))) return . M.lookup key $ o
+deepSelectKey _ _ = []
+
+main :: IO ()
+main = do
+  args <- getArgs
+  obj <- either fail return . parseJSON =<< S.getContents
+  case args of
+    [key] ->
+      S.putStrLn . showJSON . JSArray $ deepSelectKey (S8.pack key) obj
+    _ -> error "usage: json-deep-select-key <key>"
+
