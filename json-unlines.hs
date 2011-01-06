e892969dd4e861bc0f0f757896cc6411aecc236c
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2011-01-06 21:57:59 +0100

    json-unlines: add support for --help

--- json-unlines.hs
+++ json-unlines.hs
@@ -1,18 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString.Char8 as L
-import System.Environment
+import Control.Monad (when)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn,stderr)
 
 -- utility function
-getAllContents :: IO L.ByteString
-getAllContents =
-  do args <- getArgs
-     if null args
-      then L.getContents
-      else fmap L.concat . mapM file $ args
+getAllContents :: [String] -> IO L.ByteString
+getAllContents args
+  | null args = L.getContents
+  | otherwise = fmap L.concat . mapM file $ args
   where file "-" = L.getContents
         file xs  = L.readFile xs
 
+usage :: IO a
+usage = mapM_ (hPutStrLn stderr)
+  ["Usage: json-unline [--help] <file>*"
+  ,""
+  ,"Reads the given files line by line (or standard input, if '-' or"
+  ,"no file is given). Writes on standard output the a JSON array"
+  ,"made of the JSON document found on each line."]
+  >> exitFailure
+
 main :: IO ()
 main = do
-  L.putStrLn . L.cons '[' . L.intercalate "\n," . L.lines =<< getAllContents
+  args <- getArgs
+  when ("--help"`elem`args) usage
+  L.putStrLn . L.cons '[' . L.intercalate "\n," . L.lines
+    =<< getAllContents args
   L.putStrLn "]"
