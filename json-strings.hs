5acc25544550ca9b780a1178925ff6708b4b3025
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2011-01-07 15:09:05 +0100

    json-strings: --help and newline at the end

--- json-strings.hs
+++ json-strings.hs
@@ -1,7 +1,11 @@
 import Text.JSON.AttoJSON
 import qualified Data.ByteString as S
 import Data.Monoid
-import Data.Foldable
+import Data.Foldable hiding (mapM_)
+import Control.Monad (when)
+import System.IO (hPutStrLn, stderr)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
 -- NOTE that the map keys are not included
 
@@ -14,8 +18,19 @@ jsonStrings x0 = appEndo (go x0) []
         go (JSObject m) = foldMap go m
         go (JSArray xs) = foldMap go xs
 
+usage :: IO a
+usage = mapM_ (hPutStrLn stderr)
+  ["Usage: json-strings [--help]"
+  ,""
+  ,"Reads a JSON document as standard input and writes a JSON array"
+  ,"of all the strings found in the documents (expect object/mapping keys)."]
+  >> exitFailure
+
 main :: IO ()
-main = S.putStr . showJSON          -- printing the output
+main = do
+  args <- getArgs
+  when (not . null $ args) usage
+  S.putStrLn . showJSON             -- printing the output
      . JSArray . map JSString       -- building the result
      . jsonStrings                  -- main processing
      . either err id                -- error handling
