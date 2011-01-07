c3240948963a26deb33964043b0e018e4b454262
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2011-01-07 14:57:23 +0100

    json-iter,Utils: move some code to Utils

--- Utils.hs
+++ Utils.hs
@@ -16,3 +16,21 @@ readFilesS args
   | otherwise = mapM file args
   where file "-" = S.getContents
         file xs  = S.readFile xs
+
+decodeFile :: FilePath -> IO (Either String JSValue)
+decodeFile fp = parseJSON <$> S.readFile fp
+
+parseJSONFiles :: [FilePath] -> IO [JSValue]
+parseJSONFiles [] = (:[]) . either err id . parseJSON <$> S.getContents
+parseJSONFiles xs = mapM (unsafeInterleaveIO . fmap (either err id) . decodeFile) xs
+  where err :: String -> a
+        err s = error $ "JSON decoding: " ++ s
+
+systemWithStdin :: String -> S.ByteString -> IO ExitCode
+systemWithStdin shellCmd input = do
+  (Just stdinHdl, _, _, pHdl) <-
+     P.createProcess (P.shell shellCmd){ P.std_in = P.CreatePipe }
+  handle (\(_ :: IOException) -> return ()) $ do
+    S.hPutStr stdinHdl input
+    hClose stdinHdl
+  P.waitForProcess pHdl
