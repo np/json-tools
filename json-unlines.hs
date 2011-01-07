d59d8ec24d0a4ddca28293321ef8c0e9aeeceac9
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2011-01-07 10:51:06 +0100

    json-unlines: use Utils and actually use lazy bytestrings

--- json-unlines.hs
+++ json-unlines.hs
@@ -1,17 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
-import qualified Data.ByteString.Char8 as L
+import qualified Data.ByteString.Lazy.Char8 as L
 import Control.Monad (when)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn,stderr)
-
--- utility function
-getAllContents :: [String] -> IO L.ByteString
-getAllContents args
-  | null args = L.getContents
-  | otherwise = fmap L.concat . mapM file $ args
-  where file "-" = L.getContents
-        file xs  = L.readFile xs
+import Utils (readFilesL)
 
 usage :: IO a
 usage = mapM_ (hPutStrLn stderr)
@@ -26,6 +19,6 @@ main :: IO ()
 main = do
   args <- getArgs
   when ("--help"`elem`args) usage
-  L.putStrLn . L.cons '[' . L.intercalate "\n," . L.lines
-    =<< getAllContents args
+  L.putStrLn . L.cons '[' . L.intercalate "\n," . L.lines . L.concat
+    =<< readFilesL args
   L.putStrLn "]"
