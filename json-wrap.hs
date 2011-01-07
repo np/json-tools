819264a6272b39368e6360955d8ab5678cc7f0d6
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2011-01-07 15:33:47 +0100

    json-wrap: fix line ending

--- json-wrap.hs
+++ json-wrap.hs
@@ -8,7 +8,7 @@ import qualified Data.ByteString as S
 -- wrap, as in line-wrapping
 
 main :: IO ()
-main = mapM_ S.putStrLn . ($[]) . f . either err id . parseJSON =<< S.getContents
+main = mapM_ S.putStr . ($["\n"]) . f . either err id . parseJSON =<< S.getContents
   where f (JSArray a)   = t"[" . cat (intersperse (t"\n,") (map (t . showJSON) a)) . t"]"
         f (JSObject o)  = t"{" . cat (intersperse (t"\n,") (map g . toList $ o)) . t"}"
         f _             = error "impossible by decodeStrict post-condition"
