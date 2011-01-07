3628f1dee530c2cdd5569f13fa9b3336cbdb44fe
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2011-01-07 14:59:11 +0100

    json-iter: Support for --help

--- Utils.hs
+++ Utils.hs
@@ -1,7 +1,18 @@
-module Utils where
+{-# LANGUAGE ScopedTypeVariables #-}
+module Utils
+  ( readFilesL, readFilesS, decodeFile, parseJSONFiles
+  , systemWithStdin)
+where
 
+import Text.JSON.AttoJSON
+import Data.Functor
+import Control.Exception
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
+import System.IO (hClose)
+import System.Exit (ExitCode)
+import System.IO.Unsafe (unsafeInterleaveIO)
+import qualified System.Process as P
 
 readFilesL :: [String] -> IO [L.ByteString]
 readFilesL args
@@ -20,11 +31,12 @@ readFilesS args
 decodeFile :: FilePath -> IO (Either String JSValue)
 decodeFile fp = parseJSON <$> S.readFile fp
 
+decErr :: String -> a
+decErr s = error $ "JSON decoding: " ++ s
+
 parseJSONFiles :: [FilePath] -> IO [JSValue]
-parseJSONFiles [] = (:[]) . either err id . parseJSON <$> S.getContents
-parseJSONFiles xs = mapM (unsafeInterleaveIO . fmap (either err id) . decodeFile) xs
-  where err :: String -> a
-        err s = error $ "JSON decoding: " ++ s
+parseJSONFiles [] = (:[]) . either decErr id . parseJSON <$> S.getContents
+parseJSONFiles xs = mapM (unsafeInterleaveIO . fmap (either decErr id) . decodeFile) xs
 
 systemWithStdin :: String -> S.ByteString -> IO ExitCode
 systemWithStdin shellCmd input = do
--- json-iter.hs
+++ json-iter.hs
@@ -1,13 +1,8 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-import Control.Applicative
-import Control.Exception
 import System.Environment (getArgs)
-import System.Exit (ExitCode)
-import System.IO (hClose)
-import System.IO.Unsafe (unsafeInterleaveIO)
-import qualified System.Process as P
+import System.IO (hPutStrLn, stderr)
+import System.Exit (exitFailure)
+import Control.Monad (when)
 import Text.JSON.AttoJSON
-import qualified Data.ByteString as S
 import Utils (parseJSONFiles, systemWithStdin)
 
 unSequence :: JSValue -> [JSValue]
@@ -19,7 +14,19 @@ iterJSON cmd jsonfiles =
   mapM_ (mapM_ (systemWithStdin cmd . showJSON) . unSequence)
     =<< parseJSONFiles jsonfiles
 
+usage :: IO a
+usage = mapM_ (hPutStrLn stderr)
+  ["Usage: json-iter [--help] <cmd> <json-file>*"
+  ,""
+  ,"Reads the given JSON files which have to be JSON arrays, and run the given"
+  ,"command for each element. The command receive the JSON document as"
+  ,"standard input."]
+  >> exitFailure
+
 main :: IO ()
 main = do
-  cmd:args <- getArgs
-  iterJSON cmd args
+  args <- getArgs
+  when ("--help"`elem`args) usage
+  case args of
+    cmd : files -> iterJSON cmd files
+    [] -> usage
