d30557c473e4c85d3cec95a6d0b9aba625692f30
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2012-09-20 21:05:27 +0200

    Update tar2json

--- tar2json.hs
+++ tar2json.hs
@@ -1,26 +1,24 @@
 import Codec.Archive.Tar as Tar
-import Data.Convertible.Base (convertSuccess)
-import Data.Object
-import Data.Object.Json as Json
-import qualified Data.ByteString as S
-import qualified Data.ByteString.Char8 as S8
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Aeson as Aeson
+import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as L
 import System.IO
 
-tar2json :: Tar.Entries -> JsonObject
-tar2json es0 = Mapping [(k,v) | (k,Just v) <- entries es0] where
+tar2json :: Tar.Entries FormatError -> Aeson.Value
+tar2json es0 = Aeson.Object $ HM.fromList [(k,v) | (k,Just v) <- entries es0] where
 
-  entries :: Tar.Entries -> [(S.ByteString, Maybe JsonObject)]
+  entries :: Tar.Entries FormatError -> [(T.Text, Maybe Aeson.Value)]
   entries Done         = []
-  entries (Fail s)     = error s
+  entries (Fail s)     = error . show $ s
   entries (Next e es)  = (path e, contents e) : entries es
 
-  path :: Tar.Entry -> S.ByteString
-  path = S8.pack {- Is this an encoding mistake? -} . entryPath
+  path :: Tar.Entry -> T.Text
+  path = T.pack . entryPath
 
-  contents :: Tar.Entry -> Maybe JsonObject
-  contents = fmap (Scalar . JsonString) . f . entryContent where
-    f (NormalFile s _) = Just (convertSuccess s)
+  contents :: Tar.Entry -> Maybe Aeson.Value
+  contents = fmap Aeson.toJSON . f . entryContent where
+    f (NormalFile s _) = Just s
     f _                = Nothing
 
 main :: IO ()
@@ -28,8 +26,8 @@ main
  = do hPutStrLn stderr "Reading standard input..."
       hSetEncoding stdin latin1
       hSetEncoding stdout latin1
-      S.putStr            -- printing output
-       . Json.encode      -- producing JSON
+      L.putStr            -- printing output
+       . Aeson.encode     -- producing JSON
        . tar2json         -- main translation
        . Tar.read         -- reading the archive
        =<< L.getContents  -- reading the input
