a32fde45ba7aae8a27239537cf39e28d127a661e
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2012-09-20 21:57:53 +0200

    Update json-lines

--- json-lines.hs
+++ json-lines.hs
@@ -1,14 +1,16 @@
-import Text.JSON.AttoJSON
-import qualified Data.ByteString.Char8 as S
+import Data.Aeson
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
 import Control.Monad (when)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn,stderr)
-import Utils (readFilesS)
+import Utils (readFilesL)
+import qualified Data.Vector as V
 
-jsonUnlines :: JSValue -> [JSValue]
-jsonUnlines (JSArray xs) = xs
-jsonUnlines _            = error "Unexpected JSON value, only an array was expected"
+jsonUnlines :: Value -> [Value]
+jsonUnlines (Array xs) = V.toList xs
+jsonUnlines _          = error "Unexpected JSON value, only an array was expected"
 
 usage :: IO a
 usage = mapM_ (hPutStrLn stderr)
@@ -23,13 +25,13 @@ main :: IO ()
 main =
  do args <- getArgs
     when ("--help"`elem`args) usage
-    S.putStr                  -- printing the output
-     . S.unlines              -- joining lines
-     . map showJSON           -- showing each item
+    L.putStr                  -- printing the output
+     . L8.unlines             -- joining lines
+     . map encode             -- showing each item
      . concat                 -- merge lines from all files
      . map (                  -- for each file
          jsonUnlines          -- main processing
-         . either err id      -- error handling
-         . parseJSON          -- parsing each file as JSON
-       ) =<< readFilesS args  -- reading the input
-  where err msg = error $ "JSON parsing: "++msg
+         . maybe err id       -- error handling
+         . decode'            -- parsing each file as JSON
+       ) =<< readFilesL args  -- reading the input
+  where err = error "JSON decoding error"
