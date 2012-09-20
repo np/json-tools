949ff77727afdc8f1a8694168e301fc8db00b4f0
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2012-09-20 21:12:37 +0200

    Update json-xargs

--- json-xargs.hs
+++ json-xargs.hs
@@ -1,6 +1,8 @@
-import Text.JSON.AttoJSON
-import qualified Data.ByteString.Char8 as S
+import Data.Aeson
+import qualified Data.ByteString.Lazy as L
+import Data.Text as T
 import Data.Monoid
+import Data.Maybe
 import Data.Foldable
 import Data.Functor
 import System.Environment
@@ -9,20 +11,20 @@ import System.Cmd (rawSystem)
 
 -- NOTE that the map keys are not included
 
-jsonArgs :: JSValue -> [String]
+jsonArgs :: Value -> [String]
 jsonArgs x0 = appEndo (go x0) []
-  where go (JSString s) = f (S.unpack s)
-        go (JSNumber n) = f (show n)
-        go (JSBool   b) = f (if b then "true" else "false")
-        go JSNull       = f "null"
-        go (JSObject m) = foldMap go m
-        go (JSArray xs) = foldMap go xs
+  where go (String s) = f (T.unpack s)
+        go (Number n) = f (show n)
+        go (Bool   b) = f (if b then "true" else "false")
+        go Null       = f "null"
+        go (Object m) = foldMap go m
+        go (Array xs) = foldMap go xs
         f x = Endo (x:)
 
 main :: IO ()
 main = do cmd:argv <- getArgs
-          args <- jsonArgs                     -- main processing
-                . either err id                -- error handling
-                . parseJSON <$> S.getContents  -- reading the input
+          args <- jsonArgs                   -- main processing
+                . fromMaybe err              -- error handling
+                . decode' <$> L.getContents  -- reading the input
           exitWith =<< rawSystem cmd (argv ++ args)
-  where err = error . ("JSON parsing: "++)
+  where err = error "Invalid JSON input"
